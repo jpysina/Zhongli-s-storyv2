f76f28405585f5e335cde8c4b9a36200242cd9ce
jpysina <26199978+jpysina@users.noreply.github.com>
2020-12-18 06:46:45 +0900

    ok

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,13 +1,13 @@
-!/usr/bin/env sh
+#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
